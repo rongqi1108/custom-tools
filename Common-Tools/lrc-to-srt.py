e09d4d04c290aec62dbbbdb75d6c99fbc7f69c3d
RQ <1836269477@qq.com>
2024-10-27 10:24:23 +0800

    some tools for my own

--- Common-Tools/lrc-to-srt.py
+++ Common-Tools/lrc-to-srt.py
@@ -50,6 +50,4 @@ def lrc_to_srt(file_path, keep_lyrics=True):
 
     print(f"转换完成，SRT 文件保存在: {srt_path}")
 
-
-# 示例调用
 lrc_to_srt('C:\\Users\\admin\\Downloads\\Lesson 1 Budget Cuts.lrc', keep_lyrics=False)
